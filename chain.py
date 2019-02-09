8dbd83ca2d6b89131a83f3b2f5f82a3e4c2c23e3
Vincent Huynh-Watkins <vincent.huynh.watykins.45@gmail.com>
2019-02-09 12:28:26 -0800

    python script that makes sentences based on a few Trump speeches

---
+++ chain.py
@@ -0,0 +1,36 @@
+#This code comes from a tutorial on the Toward Data Science Medmium Blog
+#It is only being used to understand Markov Chains more fully and will not be
+#Part of the final project
+
+import numpy as np
+
+trump = open('speech.txt', encoding = 'utf8').read()
+
+corpus = trump.split()
+
+def make_pairs(corpus):
+	for i in range(len(corpus)-1):
+		yield(corpus[i], corpus[i+1])
+
+pairs = make_pairs(corpus)
+
+word_dict = {}
+
+for word_1, word_2 in pairs:
+	if word_1 in word_dict.keys():
+		word_dict[word_1].append(word_2)
+	else:
+		word_dict[word_1] = [word_2]
+
+first_word = np.random.choice(corpus)
+
+chain = [first_word]
+
+n_words = 30
+
+for i in range(n_words):
+	chain.append(np.random.choice(word_dict[chain[-1]]))
+
+' '.join(chain)
+
+print(chain)

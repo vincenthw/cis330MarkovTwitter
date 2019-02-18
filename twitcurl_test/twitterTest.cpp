0434f8fe97e4334084be7939545c7e854d3484e9
john <john.gauthier5@gmail.com>
2019-02-18 15:03:20 -0800

    initial test of twitcurl

---
+++ twitcurl_test/twitterTest.cpp
@@ -0,0 +1,45 @@
+#include "twitterTest.h"
+
+int main()
+{
+    twitCurl twitterObj;
+
+    twitterObj.getOAuth().setConsumerKey("KT0dbqBaPOjJNFobv09bAFwro");
+    twitterObj.getOAuth().setConsumerSecret("ChSGqvuccPdOxuc4N6sTTqs7nwxWJtbfrrv9DVXPfKfQuvjktj"); // Consumer keys taken from developer.twitter.com/apps
+
+    //*************************************************************************************************//
+    //***** This code only neccessary to run the first time to get TokenKey and TokenSecret ***********//
+    //*************************************************************************************************//    
+    // twitterObj.setTwitterUsername("chobungusPizza"); // My throwaway twitter account, lol
+    // twitterObj.setTwitterPassword("minisculebartender"); // Account password
+
+    // std::string authUrl;
+    // twitterObj.oAuthRequestToken(authUrl);
+    // twitterObj.oAuthHandlePIN(authUrl);
+
+    // twitterObj.oAuthAccessToken();
+
+    // std::string oAuthTokenKey, oAuthTokenSecret;
+    // twitterObj.getOAuth().getOAuthTokenKey(oAuthTokenKey);
+    // twitterObj.getOAuth().getOAuthTokenSecret(oAuthTokenSecret);
+    // std::cout << oAuthTokenKey << std::endl; // print the TokenKey and SecretKeys
+    // std::cout << oAuthTokenSecret << std::endl << std::endl;
+
+    twitterObj.getOAuth().setOAuthTokenKey("2554059522-W2hCWOugcPYjf15vCqoUfamWWlfOn1HNcE7Ehbp"); // Taken from the print statements above
+    twitterObj.getOAuth().setOAuthTokenSecret("MvS8YSMuphpgVNjPRnzuJOP1cxcczJXwjY7cyc0ZDItM3");
+
+    std::string replyMsg;
+    if (twitterObj.accountVerifyCredGet()) // if account credentials are valid
+    {
+        twitterObj.search("nba", "50"); // Get 50 nba tweets
+        twitterObj.getLastWebResponse(replyMsg); //set replyMsg to the twitCurl reponse
+        printf("\ntwitterClient:: twitCurl::accountVerifyCredGet web response:\n%s\n", replyMsg.c_str());
+    }
+    else
+    {
+        twitterObj.getLastCurlError(replyMsg);
+        printf("\ntwitterClient:: twitCurl::accountVerifyCredGet error:\n%s\n", replyMsg.c_str());
+    }
+
+    return 0;
+}

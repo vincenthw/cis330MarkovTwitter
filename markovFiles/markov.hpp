c811862fb73d23ee739063fc1edde8baa5b3c831
Vincent Huynh-Watkins <vincent.huynh.watykins.45@gmail.com>
2019-02-28 16:45:49 -0800

    working on markov

--- markovFiles/markov.hpp
+++ markovFiles/markov.hpp
@@ -3,11 +3,13 @@
 #include <cstdlib>
 #include <string>
 #include <unordered_map>
-
+#include<vector>
 
 using namespace std;
 
 namespace markov {
+	/*data class that the unordered map will contain to indicate
+	 count for the number of occurences */
 	class word {
 	public:
 		word(string);
@@ -16,15 +18,18 @@ namespace markov {
 		string getKey();
 		void setKey(string);
 		void increment();
+		bool operator == (const word& w); 
 	private:
 		int count;
 		string key;
 	};
-
+	
+	/* Class that will contain the actual markov chain*/
 	class markovChain {
 	public:
 		markovChain();
-		void setChain(fstream &input);
+		markovChain(fstream &);
+		markovChain(string);
 		void printChain();
 	private:
 		unordered_map<string, vector<word>>  chain;

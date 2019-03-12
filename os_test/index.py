5e2d21f733b2d635fab5de7a03bd1c60cf49c6ad
Cameron McKeown <cameronmckeown@Scotts-MacBook-Pro.local>
2019-03-12 16:42:16 -0700

    webpage added to master for linking in os_test

---
+++ os_test/index.py
@@ -0,0 +1,34 @@
+import flask
+from flask import request
+import logging
+import os
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+	return flask.render_template('twit.html')
+
+@app.route("/submit", methods=["POST"])
+def submit():
+	string1 = request.form["category"]
+	string2 = request.form["text"]
+#	app.logger.debug(string1)
+#	app.logger.debug(string2)
+	f = open("hack_test.txt", "w")
+	f.write(string1 + "\n")
+	f.write(string2)
+	f.close()
+	os.system("cat hack_test.txt")	
+	os.system("g++ test.cpp")
+	os.system("./a.out")	
+#	test.test1(string1, string2)
+	return flask.render_template('twit.html')
+app.debug = True
+if app.debug:
+    app.logger.setLevel(logging.DEBUG)
+
+if __name__ == "__main__":
+    print("Opening for global access on port {}".format(5000))
+    app.run(port=5000, host="0.0.0.0")
